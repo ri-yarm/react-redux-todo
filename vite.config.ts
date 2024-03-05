20ef58783b1c998a2f52738d88bf69298fed7948
ri_yarm <thogusus@gmail.com>
2024-03-05 23:47:05 +0500

    add: base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/react-redux-todo/",
   plugins: [
     react(),
     tsconfigPaths(),
